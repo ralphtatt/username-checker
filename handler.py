4483ce4c93bc030b1e9a0b1cf730d952bc939ba2
Ralph-Joseph Tatt <ralphtatt@gmail.com>
2022-06-10 21:19:45 +0100

    Refactor of code

--- handler.py
+++ handler.py
@@ -4,28 +4,28 @@ import time
 
 http = urllib3.PoolManager()
 
-def get_specified_usernames():
+def get_usernames():
     usernames = []
     for name in open("./usernames", "r"):
-        usernames.append(name[:-1]) # Removes newline from end
-    return usernames
+        usernames.append(name[:-1].lower()) # Removes newline from end and changes to lowercase
+    return usernames[:-1] # Removes newline from end
 
-def get_urls():
+def get_domains():
+    domains = []
+    for domain in open("./domains", "r"):
+        domains.append(domain[:-1].lower()) # Removes newline from end
+    return domains[:-1] # Removes newline from end
+
+def generate_urls(usernames, domains):
     urls = []
-    for url in open("./urls", "r"):
-        urls.append(url[:-1])
+    for username in usernames:
+        for domain in domains:
+            urls.append(domain.replace("$usercase", username))
     return urls
 
-def generate_paths(usernames, urls):
-    paths = []
-    for username in usernames:
-        for url in urls:
-            paths.append(url.replace("$USERNAME", username))
-    return paths
-
-def find_available_usernames(paths):
+def get_available_usernames(urls):
     available = []
-    for address in paths:
+    for address in urls:
         response = http.request("GET", address)
         if response.status == 404:
             available.append(address)
@@ -34,10 +34,10 @@ def find_available_usernames(paths):
 def main(event, context):
     start_time = time.time()
 
-    usernames = get_specified_usernames()
-    urls = get_urls()
-    paths = generate_paths(usernames, urls)
-    available = find_available_usernames(paths)
+    usernames = get_usernames()
+    domains = get_domains()
+    urls = generate_urls(usernames, domains)
+    available = get_available_usernames(urls)
 
     print(f"Total time: {time.time() - start_time}s")
 
